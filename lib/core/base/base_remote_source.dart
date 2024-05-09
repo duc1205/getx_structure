3381c79de0facdaf9d861292d56f56ca149a8553
duc1205 <mduc5698@gmail.com>
2024-05-09 15:00:18 +0700

    change git_ignore

--- lib/core/base/base_remote_source.dart
+++ lib/core/base/base_remote_source.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:get/get_connect/http/src/status/http_status.dart';
 import 'package:getx_structure/services/network/dio_provider.dart';
@@ -9,15 +11,32 @@ abstract class BaseRemoteDataSource {
     try {
       Response<T> response = await api;
 
-      if (response.statusCode != HttpStatus.ok ||
-          (response.data as Map<String, dynamic>)['statusCode'] !=
-              HttpStatus.ok) {
+      if (response.statusCode != HttpStatus.ok || (response.data as Map<String, dynamic>)['statusCode'] != HttpStatus.ok) {
         //
+        print(response.data);
       }
 
       return response;
     } on DioException catch (dioError) {
       // TODO: Need handle
+      switch (dioError.type) {
+        case DioExceptionType.connectionTimeout:
+          break;
+        case DioExceptionType.sendTimeout:
+        // TODO: Handle this case.
+        case DioExceptionType.receiveTimeout:
+        // TODO: Handle this case.
+        case DioExceptionType.badCertificate:
+        // TODO: Handle this case.
+        case DioExceptionType.badResponse:
+        // TODO: Handle this case.
+        case DioExceptionType.cancel:
+        // TODO: Handle this case.
+        case DioExceptionType.connectionError:
+        // TODO: Handle this case.
+        case DioExceptionType.unknown:
+        // TODO: Handle this case.
+      }
       throw dioError;
     } catch (error) {
       // TODO: Need handle
